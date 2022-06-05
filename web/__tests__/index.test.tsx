c3f44422781c4d63d76b8760ffa7a8a81eca2b72
Peter Juras <me@peterjuras.com>
2022-06-05 18:05:46 +0200

    test(web): :white_check_mark: Fix failing test

--- web/__tests__/index.test.tsx
+++ web/__tests__/index.test.tsx
@@ -1,14 +1,26 @@
-import { act, render, screen } from "@testing-library/react";
-import Home from "../pages";
+import { render, screen } from "@testing-library/react";
+import Day from "../pages/diary/[year]/[month]/[day]";
 
-describe("Home", () => {
-  it("renders the current date", () => {
-    render(<Home />);
+describe("Home Page", () => {
+  beforeAll(() => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+
+    const useRouter = jest.spyOn(require("next/router"), "useRouter");
+    useRouter.mockReturnValue({ query: { year, month, day }, isReady: true });
+  });
+
+  it("renders the current date", async () => {
+    const container = render(<Day />);
 
     const currentDate = new Intl.DateTimeFormat().format(new Date());
 
     const dateElement = screen.getByText(currentDate);
 
     expect(dateElement).toBeInTheDocument();
+
+    container.unmount();
   });
 });
