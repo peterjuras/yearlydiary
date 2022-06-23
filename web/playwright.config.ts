8f17f2904a37ebb7f85a27336f8070baf8277a5a
Peter Juras <me@peterjuras.com>
2022-06-23 21:23:34 +0200

    fix(web): :wrench: Fix playwright config

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -5,9 +5,7 @@ const config: PlaywrightTestConfig = {
     baseURL: "http://localhost:9234",
   },
   webServer: {
-    command: process.env.CI
-      ? "yarn setup-e2e-tests-ci"
-      : "yarn setup-e2e-tests",
+    command: "yarn setup-e2e-tests",
     url: "http://localhost:9234/",
     reuseExistingServer: !process.env.CI,
   },
