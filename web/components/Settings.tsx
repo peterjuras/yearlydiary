c05693eb3f50eaeef81fe1ff257b322bd2f88dba
Peter Juras <me@peterjuras.com>
2022-07-09 08:18:02 +0200

    feat(web): :lipstick: Prettify settings page

--- web/components/Settings.tsx
+++ web/components/Settings.tsx
@@ -1,4 +1,13 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Divider,
+  Flex,
+  Link,
+  LinkBox,
+  LinkOverlay,
+  Text,
+} from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
 import { useContext, useState } from "react";
 import { deleteUserData } from "../lib/client/api";
 import { UserContext } from "./UserContext";
@@ -24,10 +33,15 @@ const Settings: React.FC = () => {
   }
 
   return (
-    <Flex justify="center" direction="column">
+    <Flex justify="center" align="center" direction="column">
+      <Text alignSelf="flex-start" fontSize="sm">
+        Manage your account
+      </Text>
+      <Divider marginTop={2} marginBottom={2} />
       <Button
         disabled={!user?.userId || isBusy}
         colorScheme="red"
+        width={200}
         onClick={onDeleteDataClick}
       >
         Delete all data
@@ -35,6 +49,27 @@ const Settings: React.FC = () => {
       {showDeleteConfirmation && (
         <Text marginTop={4}>Your data has been deleted.</Text>
       )}
+      <Text marginTop={7} alignSelf="flex-start" fontSize="sm">
+        About
+      </Text>
+      <Divider marginTop={2} marginBottom={2} />
+      <Text>
+        Made with &#10084; by{" "}
+        <Link color="teal" href="https://www.peterjuras.com" isExternal>
+          Peter Juras
+        </Link>
+        .
+      </Text>
+      <LinkBox>
+        <LinkOverlay
+          href="https://github.com/peterjuras/yearlydiary"
+          isExternal
+        >
+          <Button marginTop={3} leftIcon={<FaGithub />}>
+            Fork me on GitHub
+          </Button>
+        </LinkOverlay>
+      </LinkBox>
     </Flex>
   );
 };
