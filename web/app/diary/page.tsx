5fe2b39e6afe2b10ff7b8f3d549777fd57bc768c
Peter Juras <me@peterjuras.com>
2023-01-22 21:00:31 +0100

    fix(web): :bug: Return a fragment for redirecting routes

--- web/app/diary/page.tsx
+++ web/app/diary/page.tsx
@@ -2,4 +2,5 @@ import { redirectToToday } from "../../lib/server/redirect-to-today";
 
 export default function DiaryPage() {
   redirectToToday();
+  return <></>;
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,4 +2,5 @@ import { redirectToToday } from "../lib/server/redirect-to-today";
 
 export default function RootPage() {
   redirectToToday();
+  return <></>;
 }
